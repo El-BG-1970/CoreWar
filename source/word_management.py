9dd9609a53307e448b5738a1dc037fcae054a18f
El-BG-1970 <elouan.gros.fr@gmail.com>
2020-06-01 15:26:50 +0200

    extracting sub words

--- source/word_management.py
+++ source/word_management.py
@@ -1,3 +1,12 @@
 def binpad(x, n = 0):
     assert(0 <= x)
     return '0b' + f'{x:0b}'.rjust(n, '0')
+
+def extract(w, m, n):
+    up = 2**(m+n-1)
+    lo = 2**(m)
+
+    tmp = w % up
+    tmp = tmp // lo
+
+    return tmp, bin(tmp)
