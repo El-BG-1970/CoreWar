aefafea927ecd22ef977b19ef004048b7da05fef
El-BG-1970 <elouan.gros.fr@gmail.com>
2020-06-01 15:52:31 +0200

    to_signed and of_signed

--- source/word_management.py
+++ source/word_management.py
@@ -3,10 +3,18 @@ def binpad(x, n = 0):
     return '0b' + f'{x:0b}'.rjust(n, '0')
 
 def extract(w, m, n):
-    up = 2**(m+n-1)
-    lo = 2**(m)
+    up = 2**(m+n-1)                                         # MSB of the subword
+    lo = 2**(m)                                             # LSB of the subword
 
-    tmp = w % up
-    tmp = tmp // lo
+    tmp = w % up                                            # remove all before subword
+    tmp = tmp // lo                                         # remove all after subword
 
     return tmp, bin(tmp)
+
+def to_signed(w, n):
+    return w if w&2**(n) == 0 else w - 2**(n+1)
+
+def of_signed(i, n):
+    return i + 2**(n+1) if i < 0 else i
+
+
