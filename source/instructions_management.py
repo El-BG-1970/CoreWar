3647820c60dfff49d9c73d8b3b2430e6b2995305
El-BG-1970 <elouan.gros.fr@gmail.com>
2020-06-04 13:30:08 +0200

    resolve writes now operational

--- source/instructions_management.py
+++ source/instructions_management.py
@@ -10,11 +10,30 @@ def idecode(w):
     operandB = extract(w, 20, 12)
     return (opcode, (modeA, operandA), (modeB, operandB))
 
+# def resolve_writes(base, xs):
+#     # chg = [i for i in combinations(xs, 3)] if (len(xs) % 2 == 1) else [i for i in combinations(xs+[base], 3)]
+#     chg = [i for i in combinations(xs+[base], 3)]
+#     ret = chg[0]
+#     ret = ((ret[0] & ret[1]) | (ret[1] & ret[2])) | \
+#           ((ret[1] & ret[2]) | (ret[0] & ret[2])) | \
+#           ((ret[0] & ret[1]) | (ret[0] & ret[2]))
+#     ret = sum([2**i for i in range(32)])
+#     ret = 0
+#     for i in chg:
+#         ret = ret | (((i[0] | i[1]) & (i[1] | i[2])) & \
+#                     ((i[1] | i[2]) & (i[0] | i[2])) & \
+#                     ((i[0] | i[1]) & (i[0] | i[2])))
+#     # here ret is the majority of all the elements of xs
+#     return ret
+
 def resolve_writes(base, xs):
-    chg = [i for i in combinations(xs+[base], 3)]
-    ret = chg.pop()
-    ret = ((ret[0] & ret[1]) & (ret[1] & ret[2])) | ((ret[0] & ret[1]) & (ret[0] & ret[2])) | ((ret[0] & ret[1]) & (ret[1] & ret[2]))
-    for i in chg:
-        ret = ret | ((i[0] & i[1]) & (i[1] & i[2])) | ((i[0] & i[1]) & (i[0] & i[2])) | ((i[0] & i[1]) & (i[1] & i[2]))
-    print(bin(ret))
-    return ret
+    for i in range(32):
+        ct = [0, 0]
+        for j in xs:
+            ct[extract(j, i, 1)] += 1
+        if ct[1] > ct[0]:
+            base = bit_set(base, i)
+        if ct[0] > ct[1]:
+            base = bit_clear(base, i)
+    return base
+
