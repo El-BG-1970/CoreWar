1824655d6df29e90c86d7a7eeb603ebff97f9490
El-BG-1970 <elouan.gros.fr@gmail.com>
2020-06-21 12:43:03 +0200

    now has a main for virtual machine in main.py

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-import sys
+import sys, os
 from source.machine import Machine
 
 if __name__ == "__main__":
-    with open('test1.cor', 'rb') as stream:
+    args = [str(i) for i in sys.argv[1:]]
+    if len(args) in [0,1,2]:
+        print("syntax: python corewar.py <max_turns> <program1> <program2>")
+        sys.exit(-1)
+    elif len(args) > 3:
+        print("too many arguments")
+        sys.exit(-1)
+
+    if not os.path.exists(args[1]):
+        print('invalid file for program1')
+        sys.exit(-1)
+    if not os.path.exists(args[2]):
+        print('invalid file for program2')
+        sys.exit(-1)
+    with open(args[1], 'rb') as stream:
         contents = stream.read()
     if len(contents) % 4 != 0:
         raise ValueError
@@ -11,7 +25,7 @@ if __name__ == "__main__":
         for i in range(0, len(contents), 4)
     ]
 
-    with open('test2.cor', 'rb') as stream:
+    with open(args[2], 'rb') as stream:
         contents2 = stream.read()
     if len(contents2) % 4 != 0:
         raise ValueError
@@ -20,6 +34,6 @@ if __name__ == "__main__":
         for i in range(0, len(contents2), 4)
     ]
 
-    machine = Machine(contents, contents2)
+    machine = Machine(contents, contents2, int(args[0]))
     machine.run()
     print(machine.status(), " won")
--- source/machine.py
+++ source/machine.py
@@ -57,7 +57,7 @@ class Machine:
                 self.step()
                 a = self.status()
                 ct += 1
-            return a | 0
+            return a
         while not a:
             self.step()
             a = self.status()
